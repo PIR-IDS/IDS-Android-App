dd34a74991dc41c121a579836a24ba32250aa3f1
rmonier <romain.monier0609@gmail.com>
2022-03-03 11:53:42 +0100

    Init android app

---
+++ README.md
@@ -0,0 +1,1 @@
+# IDS ANDROID APP
---
+++ app/src/test/java/fr/pirids/idsapp/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package fr.pirids.idsapp
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
