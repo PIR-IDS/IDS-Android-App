30b4bc9d6bc0cc3d4b9032bbc4b31e2efd4a29e8
rmonier <rmonier@laas.fr>
2022-06-24 09:04:41 +0200

    Cleaned Color file

--- app/src/main/java/fr/pirids/idsapp/ui/theme/Color.kt
+++ app/src/main/java/fr/pirids/idsapp/ui/theme/Color.kt
@@ -32,12 +32,12 @@ val md_theme_light_shadow = Color(0xFF000000)
 val md_theme_light_surfaceTint = Color(0xFF979797)
 val md_theme_light_surfaceTintColor = Color(0xFF808080)
 
-val md_theme_dark_primary = Color(0xFF121212)  //
-val md_theme_dark_onPrimary = Color(0xFFf5f5f5)  //
-val md_theme_dark_primaryContainer = Color(0xFF73451f)  //
-val md_theme_dark_onPrimaryContainer = Color(0xFFfffcdd)  //
+val md_theme_dark_primary = Color(0xFF121212)
+val md_theme_dark_onPrimary = Color(0xFFf5f5f5)
+val md_theme_dark_primaryContainer = Color(0xFF73451f)
+val md_theme_dark_onPrimaryContainer = Color(0xFFfffcdd)
 val md_theme_dark_secondary = Color(0xFFB6B6B6)
-val md_theme_dark_onSecondary = Color(0xFF1D1D1D)  // 0xFF363636
+val md_theme_dark_onSecondary = Color(0xFF1D1D1D)
 val md_theme_dark_secondaryContainer = Color(0xFF313131)
 val md_theme_dark_onSecondaryContainer = Color(0xFFf5f5f5)
 val md_theme_dark_tertiary = Color(0xFF4E4A45)
@@ -48,10 +48,10 @@ val md_theme_dark_error = Color(0xFFFFB4AB)
 val md_theme_dark_errorContainer = Color(0xFF93000A)
 val md_theme_dark_onError = Color(0xFF690005)
 val md_theme_dark_onErrorContainer = Color(0xFFFFDAD6)
-val md_theme_dark_background = Color(0xFF2E2E2E) //
-val md_theme_dark_onBackground = Color(0xFFf5f5f5)  //
-val md_theme_dark_surface = Color(0xFF2a2a2a)  //
-val md_theme_dark_onSurface = Color(0xFFfffcdd) //
+val md_theme_dark_background = Color(0xFF2E2E2E)
+val md_theme_dark_onBackground = Color(0xFFf5f5f5)
+val md_theme_dark_surface = Color(0xFF2a2a2a)
+val md_theme_dark_onSurface = Color(0xFFfffcdd)
 val md_theme_dark_surfaceVariant = Color(0xFF4E4A45)
 val md_theme_dark_onSurfaceVariant = Color(0xFFf5f5f5)
 val md_theme_dark_outline = Color(0xFF99948F)
@@ -65,7 +65,8 @@ val md_theme_dark_surfaceTintColor = Color(0xFFf5f5f5)
 
 val seed = Color(0xFF777777)
 
-val custom_dark_success = Color(0xFF48D5A2)
-val custom_dark_onSuccess = Color(0xFFFFFFFF)
 val custom_light_success = Color(0xFFAACEC1)
 val custom_light_onSuccess = Color(0xFFFFFFFF)
+
+val custom_dark_success = Color(0xFF48D5A2)
+val custom_dark_onSuccess = Color(0xFFFFFFFF)
