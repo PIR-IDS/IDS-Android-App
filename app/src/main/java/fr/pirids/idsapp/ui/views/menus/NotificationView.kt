019174c2bec3ef870681309885d1339a58d0337a
amuller <amelie17.muller@gmail.com>
2022-07-07 09:51:39 +0200

    notificationView modified

--- app/src/main/java/fr/pirids/idsapp/ui/views/menus/NotificationView.kt
+++ app/src/main/java/fr/pirids/idsapp/ui/views/menus/NotificationView.kt
@@ -1,31 +1,41 @@
-@file:OptIn(ExperimentalAnimationApi::class, ExperimentalMaterial3Api::class)
+@file:OptIn(ExperimentalAnimationApi::class, ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class)
 
 package fr.pirids.idsapp.ui.views.menus
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import android.util.Log
 import fr.pirids.idsapp.R
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.DismissValue
+import androidx.compose.material.*
 import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.outlined.ArrowBack
 import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material3.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ComposeCompilerApi
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color.Companion.Transparent
+import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -45,70 +55,135 @@ fun NotificationView(navController: NavHostController) {
     ) {
         Scaffold(
             topBar = { TopBar(navController) }
-        ) {
-            Column(
+        ){
+            LazyColumn(
                 modifier = Modifier
-                    .fillMaxSize()
-                    .verticalScroll(rememberScrollState())
                     .padding(top = it.calculateTopPadding()),
                 horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(15.dp),
-                    onClick = { NotificationViewController.goBack(navController)
-                    }
-                ) {
-                    Row {
-                                Column {
-                                    Text(
-                                        text = stringResource(id = R.string.wallet_intrusion),
-                                        color = MaterialTheme.colorScheme.onBackground,
-                                        modifier = Modifier
-                                            .padding(horizontal = 35.dp, vertical = 8.dp),
-                                        textAlign = TextAlign.Center,
-                                        style = MaterialTheme.typography.bodyLarge
-                                    )
-                                    Text(
-                                        text = stringResource(id = R.string.suspicious_transaction),
-                                        color = MaterialTheme.colorScheme.onBackground,
-                                        modifier = Modifier
-                                            .padding(horizontal = 40.dp, vertical = 0.5.dp),
-                                        textAlign = TextAlign.Center,
-                                        style = MaterialTheme.typography.bodySmall
-                                    )
-                                }
 
-                            Spacer(modifier = Modifier.height(15.dp))
-                            Spacer(modifier = Modifier
-                                .weight(1f)
-                                .fillMaxHeight())
-                        Box(
-                            modifier = Modifier
-                                .size(70.dp),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            val izlyTest = ServiceItem.list.first()
-                            Image(
-                                painter = painterResource(id = izlyTest.logo),
-                                contentDescription = izlyTest.name,
-                                //contentScale = ContentScale.Crop,
-                                modifier = Modifier
-                                    .size(60.dp)
-                                    .clip(CircleShape)
-                                    .clickable(
-                                        enabled = true,
-                                        onClickLabel = izlyTest.name,
-                                        onClick = { }
-                                    )
-                            )
-                        }
-                    }
+            ){
+                items(listOf("aaa", "a", "a", "a")) {
+                    val dismissState = rememberDismissState(initialValue = DismissValue.Default)
+                    SwipeToDismiss(
+                        state = dismissState,
+                        background = {
+                            val color = when (dismissState.dismissDirection) {
+                                DismissDirection.StartToEnd -> MaterialTheme.colorScheme.background
+                                DismissDirection.EndToStart -> MaterialTheme.colorScheme.background
+                                null -> MaterialTheme.colorScheme.background
+                            }
+                            val direction = dismissState.dismissDirection
+                            if (direction == DismissDirection.StartToEnd) {
+                                Box(
+                                    modifier = Modifier
+                                        .fillMaxSize()
+                                        .padding(40.dp)
+                                ) {
+                                    Column(modifier = Modifier.align(Alignment.CenterStart)) {
+                                        Icon(
+                                            imageVector = Icons.Default.Delete,
+                                            contentDescription = null,
+                                            tint = MaterialTheme.colorScheme.onBackground,
+                                            modifier = Modifier.align(Alignment.CenterHorizontally)
+                                        )
+                                        Spacer(modifier = Modifier.heightIn(5.dp))
+                                        Text(
+                                            text = stringResource(id = R.string.delete),
+                                            textAlign = TextAlign.Center,
+                                            color = MaterialTheme.colorScheme.onBackground
+                                        )
+                                    }
+                                }
+                            } else {
+                                Box(
+                                    modifier = Modifier
+                                        .fillMaxSize()
+                                        .padding(15.dp)
+                                ) {
+                                    Column(modifier = Modifier.align(Alignment.CenterEnd)) {
+                                        Icon(
+                                            imageVector = Icons.Default.Delete,
+                                            contentDescription = null,
+                                            tint = MaterialTheme.colorScheme.onBackground,
+                                            modifier = Modifier.align(Alignment.CenterHorizontally)
+                                        )
+                                        Spacer(modifier = Modifier.heightIn(5.dp))
+                                        Text(
+                                            text = stringResource(id = R.string.delete),
+                                            textAlign = TextAlign.Center,
+                                            color = MaterialTheme.colorScheme.onBackground
+                                        )
+                                    }
+                                }
+                            }
+                        },
+                        dismissContent = {
+                            NotificationCard(navController = navController)
+                        },
+                        directions = setOf(DismissDirection.EndToStart)
+                    )
                 }
             }
         }
     }
+}
+
+
+@Composable
+fun NotificationCard(navController: NavHostController) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            onClick = {
+                NotificationViewController.goBack(navController)
+            }
+        ) {
+            Row {
+                Column {
+                    Text(
+                        text = stringResource(id = R.string.wallet_intrusion),
+                        color = MaterialTheme.colorScheme.onBackground,
+                        modifier = Modifier
+                            .padding(horizontal = 35.dp, vertical = 8.dp),
+                        textAlign = TextAlign.Center,
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+                    Text(
+                        text = stringResource(id = R.string.suspicious_transaction),
+                        color = MaterialTheme.colorScheme.onBackground,
+                        modifier = Modifier
+                            .padding(horizontal = 40.dp, vertical = 0.5.dp),
+                        textAlign = TextAlign.Center,
+                        style = MaterialTheme.typography.bodySmall
+                    )
+                }
+                Spacer(modifier = Modifier.height(15.dp))
+                Spacer(modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight())
+                Box(
+                    modifier = Modifier
+                        .size(70.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    val izlyTest = ServiceItem.list.first()
+                    Image(
+                        painter = painterResource(id = izlyTest.logo),
+                        contentDescription = izlyTest.name,
+                        //contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .size(60.dp)
+                            .clip(CircleShape)
+                            .clickable(
+                                enabled = true,
+                                onClickLabel = izlyTest.name,
+                                onClick = { }
+                            )
+                    )
+                }
+            }
+        }
 }
 
 @Preview
