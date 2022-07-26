3afe9c5a65194e170d1c7ab0fd6d1d74e54a317e
rmonier <rmonier@laas.fr>
2022-07-26 13:59:47 +0200

    Settings preferences now activated

--- app/src/main/java/fr/pirids/idsapp/controller/view/menus/SettingsViewController.kt
+++ app/src/main/java/fr/pirids/idsapp/controller/view/menus/SettingsViewController.kt
@@ -1,7 +1,36 @@
 package fr.pirids.idsapp.controller.view.menus
 
+import android.content.Context
+import androidx.compose.runtime.mutableStateOf
 import androidx.navigation.NavHostController
+import fr.pirids.idsapp.data.preferences.UserPreferencesRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 object SettingsViewController {
+    val fullScreenState = mutableStateOf(UserPreferencesRepository.DEFAULT_FULLSCREEN_NOTIFICATION)
+
+    fun initPreferences(context: Context) {
+        CoroutineScope(Dispatchers.IO).launch {
+            updateFullScreenState(context)
+        }
+    }
+
     fun goBack(navController: NavHostController) = navController.popBackStack()
+
+    suspend fun toggleFullScreenAlert(context: Context, value: Boolean) {
+        UserPreferencesRepository(context).updateFullscreenNotification(value)
+    }
+
+    private suspend fun updateFullScreenState(context: Context) {
+        try {
+            val value = UserPreferencesRepository(context).userPreferencesFlow.first().fullscreenNotification
+            withContext(Dispatchers.Main) {
+                fullScreenState.value = value
+            }
+        } catch (e: NoSuchElementException) { }
+    }
 }
--- app/src/main/java/fr/pirids/idsapp/ui/views/menus/SettingsView.kt
+++ app/src/main/java/fr/pirids/idsapp/ui/views/menus/SettingsView.kt
@@ -15,8 +15,10 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -27,11 +29,14 @@ import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import fr.pirids.idsapp.BuildConfig
 import fr.pirids.idsapp.R
 import fr.pirids.idsapp.controller.view.menus.SettingsViewController
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @Composable
 fun SettingsView(navController: NavHostController) {
-    val notificationState = remember { mutableStateOf(true) }
-    val fullScreenState = remember { mutableStateOf(true) }
+    val context = LocalContext.current
+    SettingsViewController.initPreferences(context)
     Surface(
         color = MaterialTheme.colorScheme.background
     ) {
@@ -54,44 +59,25 @@ fun SettingsView(navController: NavHostController) {
                     textAlign = TextAlign.Center,
                     style = MaterialTheme.typography.headlineMedium
                 )
-                Spacer(modifier = Modifier.height(20.dp))
-                Row {
-                    Text(
-                        text = stringResource(id = R.string.notifications),
-                        color = MaterialTheme.colorScheme.onBackground,
-                        modifier = Modifier
-                            .padding(horizontal = 35.dp, vertical = 7.dp),
-                        textAlign = TextAlign.Center,
-                        style = MaterialTheme.typography.bodyLarge
-                    )
-                    Spacer(
-                        modifier = Modifier
-                            .weight(1f)
-                            .fillMaxHeight()
-                    )
-                    Switch(
-                        checked = notificationState.value,
-                        onCheckedChange = { value -> notificationState.value = value },
-                        modifier = Modifier.scale(0.7f)
-                    )
-                }
                 Spacer(modifier = Modifier.height(15.dp))
-                Row {
-                    Column {
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .padding(horizontal = 35.dp, vertical = 8.dp)
+                    ) {
                         Text(
                             text = stringResource(id = R.string.full_screen_alert),
                             color = MaterialTheme.colorScheme.onBackground,
-                            modifier = Modifier
-                                .padding(horizontal = 35.dp, vertical = 8.dp),
-                            textAlign = TextAlign.Center,
+                            textAlign = TextAlign.Start,
                             style = MaterialTheme.typography.bodyLarge
                         )
                         Text(
                             text = stringResource(id = R.string.full_screen_alert_explanation),
                             color = MaterialTheme.colorScheme.onBackground,
-                            modifier = Modifier
-                                .padding(horizontal = 40.dp, vertical = 0.5.dp),
-                            textAlign = TextAlign.Center,
+                            textAlign = TextAlign.Start,
                             style = MaterialTheme.typography.bodySmall
                         )
                     }
@@ -101,9 +87,13 @@ fun SettingsView(navController: NavHostController) {
                             .fillMaxHeight()
                     )
                     Switch(
-                        checked = fullScreenState.value,
-                        onCheckedChange = { value -> fullScreenState.value = value },
-                        modifier = Modifier.scale(0.7f),
+                        checked = SettingsViewController.fullScreenState.value,
+                        onCheckedChange = { value ->
+                            SettingsViewController.fullScreenState.value = value
+                            CoroutineScope(Dispatchers.IO).launch {
+                                SettingsViewController.toggleFullScreenAlert(context, value)
+                            }
+                        }
                     )
                 }
                 Spacer(modifier = Modifier.height(30.dp))
@@ -117,42 +107,43 @@ fun SettingsView(navController: NavHostController) {
                     style = MaterialTheme.typography.headlineMedium
                 )
                 Spacer(modifier = Modifier.height(20.dp))
-                Column {
-                    Text(
-                        text = stringResource(id = R.string.version),
-                        color = MaterialTheme.colorScheme.onBackground,
-                        modifier = Modifier
-                            .padding(horizontal = 35.dp, vertical = 8.dp),
-                        textAlign = TextAlign.Center,
-                        style = MaterialTheme.typography.bodyLarge
-                    )
-                    Text(
-                        text = BuildConfig.VERSION_NAME,
-                        color = MaterialTheme.colorScheme.onBackground,
-                        modifier = Modifier
-                            .padding(horizontal = 45.dp),
-                        textAlign = TextAlign.Center,
-                        style = MaterialTheme.typography.bodySmall
-                    )
-                }
-                Spacer(modifier = Modifier.height(20.dp))
-                Column {
-                    Text(
-                        text = stringResource(id = R.string.developers),
-                        color = MaterialTheme.colorScheme.onBackground,
-                        modifier = Modifier
-                            .padding(horizontal = 35.dp, vertical = 8.dp),
-                        textAlign = TextAlign.Center,
-                        style = MaterialTheme.typography.bodyLarge
-                    )
-                    Text(
-                        text = "Romain Monier, Morgan Pelloux, Noé Chauveau, Amélie Muller",
-                        color = MaterialTheme.colorScheme.onBackground,
-                        modifier = Modifier
-                            .padding(horizontal = 45.dp),
-                        textAlign = TextAlign.Left,
-                        style = MaterialTheme.typography.bodySmall
-                    )
+                Column(
+                    modifier = Modifier
+                        .padding(horizontal = 35.dp, vertical = 8.dp),
+                ) {
+                    Column {
+                        Text(
+                            text = stringResource(id = R.string.version),
+                            color = MaterialTheme.colorScheme.onBackground,
+                            textAlign = TextAlign.Center,
+                            style = MaterialTheme.typography.bodyLarge
+                        )
+                        Text(
+                            text = BuildConfig.VERSION_NAME,
+                            color = MaterialTheme.colorScheme.onBackground,
+                            modifier = Modifier
+                                .padding(horizontal = 10.dp, vertical = 8.dp),
+                            textAlign = TextAlign.Center,
+                            style = MaterialTheme.typography.bodySmall
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(20.dp))
+                    Column {
+                        Text(
+                            text = stringResource(id = R.string.developers),
+                            color = MaterialTheme.colorScheme.onBackground,
+                            textAlign = TextAlign.Center,
+                            style = MaterialTheme.typography.bodyLarge
+                        )
+                        Text(
+                            text = "Romain Monier, Morgan Pelloux, Noé Chauveau, Amélie Muller",
+                            color = MaterialTheme.colorScheme.onBackground,
+                            modifier = Modifier
+                                .padding(horizontal = 10.dp, vertical = 8.dp),
+                            textAlign = TextAlign.Left,
+                            style = MaterialTheme.typography.bodySmall
+                        )
+                    }
                 }
             }
         }
